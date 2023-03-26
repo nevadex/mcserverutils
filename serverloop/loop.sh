d263bb6ce61c1a0b0da6f159480c3851afa350d6
nevadex <80215914+nevadex@users.noreply.github.com>
2023-03-25 22:03:30 -0400

    Create loop.sh

---
+++ serverloop/loop.sh
@@ -0,0 +1,8 @@
+while :
+do
+# any script to run before server starts goes here
+for P in {5..1} ; do echo "starting in ${P}" ; sleep 1 ; done
+echo starting..
+# minecraft server command here
+# any script to run after server stops goes here
+done
